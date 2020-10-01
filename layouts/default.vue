283c0975ca455b9b9d8bfb7f586f8c3366be0f5d
Alexandre <alexandre@MacBook-Pro.local>
2020-10-01 21:23:32 +0200

    Add hello world

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,10 @@
 <script>
+export default {};
 </script>
 <template>
+  <div id="app">
+    <nuxt />
   </div>
 </template>
+<style lang="scss">
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,28 @@
 <script>
+import Hello from "../components/Hello";
 
 export default {
   components: {
+    hello: Hello,
+  },
 };
 </script>
 <template>
+  <div class="main home">
+    <section class="landing">
+      <hello />
+    </section>
   </div>
 </template>
 <style lang="scss">
+.home {
+  .landing {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    
+  }
 }
 </style>
