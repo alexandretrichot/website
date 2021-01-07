fcd0ee5658c552aba9bfb63052458bfb74da2697
Alexandre TRICHOT <contact@alexandretrichot.fr>
2021-01-07 10:16:39 +0100

    Clean and setup react project

--- plugins/player/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import Player from './Player';
-
-Vue.use({
-  install(vueInst) {
-    vueInst.prototype.$player = new Player();
-  }
-});
--- store/index.js
+++
@@ -1,9 +0,0 @@
-export const state = () => ({
-  menuOpen: false
-});
-
-export const mutations = {
-  toggleMenu(state) {
-    state.menuOpen = !state.menuOpen;
-  }
-}
