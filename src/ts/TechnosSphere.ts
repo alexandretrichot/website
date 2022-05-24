d7a067efa88a6f686884e8eb2918eff84271d102
Alexandre TRICHOT <contact@alexandretrichot.fr>
2022-05-24 18:18:15 +0200

    perf: improvements

--- src/ts/TechnosSphere.ts
+++ src/ts/TechnosSphere.ts
@@ -3,7 +3,7 @@ import Smooter from './Smoother';
 import ImVisible from './ImVisible';
 
 export default class TechnosSphere extends THREE.Object3D {
-  private center = new THREE.Mesh(new THREE.IcosahedronGeometry(.7, 2), new THREE.MeshLambertMaterial({ wireframe: true }))
+  private center = new THREE.Mesh(new THREE.IcosahedronGeometry(0.7, 2), new THREE.MeshLambertMaterial({ wireframe: true }));
   private images = new THREE.Object3D();
 
   private imVisible = new ImVisible(document.getElementById('about')!, {
@@ -31,18 +31,18 @@ export default class TechnosSphere extends THREE.Object3D {
     const a = 1 / PHI;
     const b = a / PHI;
     const vertices: number[][] = [
-      [-b, 0, a,],
-      [b, 0, a,],
-      [-b, 0, -a,],
-      [b, 0, -a,],
-      [0, a, b,],
-      [0, a, -b,],
-      [0, -a, b,],
-      [0, -a, -b,],
-      [a, b, 0,],
-      [-a, b, 0,],
-      [a, -b, 0,],
-      [-a, -b, 0,],
+      [-b, 0, a],
+      [b, 0, a],
+      [-b, 0, -a],
+      [b, 0, -a],
+      [0, a, b],
+      [0, a, -b],
+      [0, -a, b],
+      [0, -a, -b],
+      [a, b, 0],
+      [-a, b, 0],
+      [a, -b, 0],
+      [-a, -b, 0],
     ];
 
     const technos: Promise<any>[] = [
@@ -60,16 +60,24 @@ export default class TechnosSphere extends THREE.Object3D {
       import('../images/techno/javascript.svg?url'),
     ];
 
-    Promise.all(technos).then(modules => modules.map(m => m.default).forEach((url, index) => {
-      new THREE.TextureLoader().load(url, texture => {
-        document.getElementById('hidden')!.appendChild(texture.image);
-        const plane = new THREE.Mesh(new THREE.PlaneGeometry(.15, .15 * texture.image.height / texture.image.width), new THREE.MeshLambertMaterial({ map: texture, transparent: true }));
-        //const plane = new THREE.Mesh(new THREE.PlaneGeometry(.15, .15), new THREE.MeshNormalMaterial());
-        //const plane = new THREE.Mesh(new THREE.BoxGeometry(.15, .15, .15), new THREE.MeshLambertMaterial({ map: texture, transparent: true }));
-        plane.position.set(vertices[index][0], vertices[index][1], vertices[index][2]);
-        this.images.add(plane);
-      })
-    }));
+    Promise.all(technos).then((modules) =>
+      modules
+        .map((m) => m.default)
+        .forEach((url, index) => {
+          new THREE.TextureLoader().load(url, (texture) => {
+            texture.image.setAttribute('aria-hidden', 'true');
+            document.getElementById('hidden')!.appendChild(texture.image);
+            const plane = new THREE.Mesh(
+              new THREE.PlaneGeometry(0.15, (0.15 * texture.image.height) / texture.image.width),
+              new THREE.MeshLambertMaterial({ map: texture, transparent: true })
+            );
+            //const plane = new THREE.Mesh(new THREE.PlaneGeometry(.15, .15), new THREE.MeshNormalMaterial());
+            //const plane = new THREE.Mesh(new THREE.BoxGeometry(.15, .15, .15), new THREE.MeshLambertMaterial({ map: texture, transparent: true }));
+            plane.position.set(vertices[index][0], vertices[index][1], vertices[index][2]);
+            this.images.add(plane);
+          });
+        })
+    );
   }
 
   scroll() {
@@ -82,14 +90,14 @@ export default class TechnosSphere extends THREE.Object3D {
       this.scale.set(1, 1, 1);
     } else {
       this.position.x = 0.5;
-      this.scale.set(.5, .5, .5);
+      this.scale.set(0.5, 0.5, 0.5);
     }
   }
 
   render(time: number) {
     this.position.z = this.zPos.value;
     this.images.rotation.set(Math.sin(time * 0.0003), time * 0.0005, 0);
-    this.images.children.forEach(m => {
+    this.images.children.forEach((m) => {
       m.lookAt(new THREE.Vector3(0, 0, 50));
     });
     this.center.rotation.y = -1 * time * 0.0004;
